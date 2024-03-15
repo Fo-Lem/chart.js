e2b33fdac6a39a2fffdde58c4d75c8cfb9fd56ec
Fo-lem <darksunset2001@gmail.com>
2024-03-15 11:50:09 +0300

    добавил pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/ru'
 import '@quasar/extras/roboto-font/roboto-font.css'
@@ -6,12 +7,14 @@ import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import App from './App.vue'
 
-const myApp = createApp(App)
+const pinia = createPinia()
+const app = createApp(App)
 
-myApp.use(Quasar, {
+app.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
   lang: quasarLang,
 })
 
 // Assumes you have a <div id="app"></div> in your index.html
-myApp.mount('#app')
+app.use(pinia)
+app.mount('#app')
