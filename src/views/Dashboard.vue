d1267dbc39f58fd011fcd840a7485e7f12bab858
Fo-lem <darksunset2001@gmail.com>
2024-03-18 13:35:20 +0300

    переход на apexcharm

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,40 +1,54 @@
 <template>
-  <div class=" max-w-7xl mx-auto grid grid-cols-2 gap-5">
-    <div>
-      <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
-    </div>
-    <div>
-      <Doughnut id="my-doughnut-id" :options="DoughnutOptions" :data="DoughnutData" />
-    </div>
+  <div class="max-w-7xl mx-auto gap-5">
+    <apexchart width="100%" height="380" type="bar" :series="dataBar" :options="options"></apexchart>
+    <apexchart width="100%" height="380" type="line" :series="dataBar" :options="options"></apexchart>
+    <apexchart width="100%" height="380" type="boxPlot" :series="dataBoxPlot" :options="options"></apexchart>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Bar, Doughnut } from 'vue-chartjs'
+const dataBar = [
+  {
+    name: 'Набор данных 1',
+    data: [44, 55, 41, 64, 22, 43, 21, 12],
+  },
+  {
+    name: 'Набор данных 2',
+    data: [30, 40, 45, 50, 49, 60, 70, 91],
+  },
+];
 
-import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
+const dataBoxPlot = [{
+  data: [{
+    x: "category 1",
+    y: [40, 51.98, 56.29, 59.59, 63.85]
+  },
+  {
+    x: "category 2",
+    y: [43.66, 44.99, 51.35, 52.95, 59.42]
+  }]
+}]
 
-const chartData = {
-  labels: ['January', 'February', 'March'],
-  datasets: [{
-    label: 'Data One',
-    data: [40, 20, 12]
-  }],
-};
-const chartOptions = {
-  responsive: true,
-};
-
-const DoughnutData = {
-  labels: ['January', 'February', 'March'],
-  datasets: [{
-    label: 'Data One',
-    data: [40, 20, 12]
-  }],
-};
-const DoughnutOptions = {
-  responsive: true,
+const options = {
+  xaxis: {
+    categories: [
+      'Korea',
+      'Canada',
+      'Poland',
+      'Italy',
+      'France',
+      'Japan',
+      'China',
+    ],
+  },
+  legend: {
+    position: 'right',
+    verticalAlign: 'top',
+    containerMargin: {
+      left: 35,
+      right: 60,
+    },
+  },
 };
 </script>
 
